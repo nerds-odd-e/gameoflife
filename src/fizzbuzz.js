2baa54bd607bed67dd96f6c43c8e744914d58077
zbcjackson <zbcjackson@gmail.com>
2020-01-13 15:01:06 +1100

    empty message

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,10 +1,10 @@
 module.exports = {
   say(number) {
-    let match = [{factor: 15, word: 'FizzBuzz'}].find(rule => number % rule.factor === 0)
+    let match = [{factor: 15, word: 'FizzBuzz'}, {factor: 5, word: 'Buzz'}].find(rule => number % rule.factor === 0)
     if (match){
       return match.word
     }
-    if (number % 5 === 0) {return 'Buzz'}
+    //if (number % 5 === 0) {return 'Buzz'}
     if (number % 3 === 0) {return 'Fizz'}
     return number.toString()
   }
