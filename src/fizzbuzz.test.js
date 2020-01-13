e9331e527af625f13d9fdaab7eea66551fc14c34
Joey Chen <jay.hatelove@gmail.com>
2020-01-13 11:39:26 +0800

    working

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -2,7 +2,7 @@ const { fizzbuzz } = require("./fizzbuzz");
 
 describe("fizzbuzz", () => {
   it("get 1 when input 1", () => {
-    expect('1').toEqual('1')
+    expect('1').toEqual('1');
 
   });
 
