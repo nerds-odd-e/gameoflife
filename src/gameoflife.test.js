e0a1133303e10b96db1e9423ea95f72a5114849d
Terry Yin <terry.yinzhe@gmail.com>
2020-01-13 14:11:54 +1100

    working

--- src/gameoflife.test.js
+++ src/gameoflife.test.js
@@ -2,8 +2,6 @@ const { x, game_of_life, count_neighbours } = require("./gameoflife");
 
 describe("count neighbours", () => {
   it("empty", () => {
-    expect("game").not.toBeUndefined();
-    //expect(count_neighbours()).toEqual(42);
   });
   it("given 3 cells", () => {
     let a_list_of_positions = [[1, 2], [3, 4], [5, 6]];
