4d2a586d3fad0000e7d7ea65da15312fc2f8edd7
Terry Yin <terry.yinzhe@gmail.com>
2020-01-13 14:12:02 +1100

    working

--- src/gameoflife.test.js
+++ src/gameoflife.test.js
@@ -3,8 +3,5 @@ const { x, game_of_life, count_neighbours } = require("./gameoflife");
 describe("count neighbours", () => {
   it("empty", () => {
   });
-  it("given 3 cells", () => {
-    let a_list_of_positions = [[1, 2], [3, 4], [5, 6]];
-    game_of_life(a_list_of_positions);
-  })
+
 });
