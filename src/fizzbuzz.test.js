ef9e1c17a449efb2620db64c88629dea5b63e84a
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2020-01-15 07:54:52 +0800

    empty message

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -4,6 +4,7 @@ describe('FizzBuzz', () => {
   it('says number for normal number', () => {
     expect(fizzbuzz.say(1)).toBe('1')
     expect(fizzbuzz.say(2)).toBe('2')
+    expect(fizzbuzz.say(4)).toBe('4')
   })
 
   it('says Fizz for 3x', () => {
