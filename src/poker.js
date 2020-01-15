9e36ec036395594054ee14020e06a7c5fe5c5244
zbcjackson <zbcjackson@gmail.com>
2020-01-15 11:44:59 +1100

    empty message

--- src/poker.js
+++ src/poker.js
@@ -1,14 +1,16 @@
 module.exports = {
-    check(c1, c2) {
+    extracted: function (player1_hands, player2_hands) {
+        if (player1_hands[3] > player2_hands[3]) {
+            return "Black wins"
+        }
+        return "White wins"
+    }, check(c1, c2) {
         const player1_hands = c1.split(',').sort()
         const player2_hands = c2.split(',').sort()
         if (c1 === c2)
             return 'Tie';
         if (player1_hands[4] === player2_hands[4]) {
-            if(player1_hands[3] > player2_hands[3]){
-                return "Black wins"
-            }
-            return "White wins"
+            return this.extracted(player1_hands, player2_hands);
         }
         if(player1_hands[4] > player2_hands[4]){
             return "Black wins"
