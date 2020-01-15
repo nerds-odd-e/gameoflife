1f0a5d5b7801132fab79a6a43bab836c7eefa8e9
Terry Yin <terry.yinzhe@gmail.com>
2020-01-15 11:26:06 +1100

    working

--- src/poker.test.js
+++ src/poker.test.js
@@ -1,10 +1,6 @@
 let poker =require('./poker.js')
 
 describe('Pocker hand', () => {
-  it('both players have four of a kind', () => {
-    expect(poker.check('2S,2H,2C,2D,3D', '2S,2H,2C,2D,3D')).toBe('Tie')
-  })
-
   it('High card white win', () => {
     expect(poker.check('3S,2S,4S,5S,6D', '3S,2S,4S,5S,7D')).toBe('White wins')
   })
@@ -15,5 +11,9 @@ describe('Pocker hand', () => {
 
   it('High card black win by comparing number only', () => {
     expect(poker.check('3S,2S,4S,5S,7C', '3S,2S,4S,5S,6D')).toBe('Black wins')
+  })
+
+  it('both players have four of a kind', () => {
+    expect(poker.check('2S,2H,2C,2D,3D', '2S,2H,2C,2D,3D')).toBe('Tie')
   })
 })
