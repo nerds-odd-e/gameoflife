96c602bc5fd4ba4607bea3081d9ecebaf601b7d7
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2020-01-15 08:18:53 +0800

    empty message

--- src/poker.js
+++ src/poker.js
@@ -1,7 +1,7 @@
 module.exports = {
     check(c1, c2) {
-        let player1_hands = c1.split(',')
-        let player2_hands = c2.split(',')
+        const player1_hands = c1.split(',')
+        const player2_hands = c2.split(',')
         if (c1 === c2)
             return 'Tie';
         return "White wins"
