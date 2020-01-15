a1f51f06455bcd6d960f293385ed07af34c1eb37
Joey Chen <jay.hatelove@gmail.com>
2020-01-15 12:18:59 +1100

    working

--- src/poker.js
+++ src/poker.js
@@ -13,7 +13,7 @@ const highCardWins = (player1_hands, player2_hands, index) => {
     if (card(player1_hands[index]).value === card(player2_hands[index]).value) {
         return highCardWins(player1_hands, player2_hands, index - 1)
     }
-    if (player1_hands[index] > player2_hands[index]) {
+    if (card(player1_hands[index]).value > card(player2_hands[index]).value) {
         return "Black wins"
     }
     return "White wins"
