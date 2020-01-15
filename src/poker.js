50a3530fc413376a1a55f114e21ff4c1a40c89ab
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2020-01-15 08:21:58 +0800

    empty message

--- src/poker.js
+++ src/poker.js
@@ -4,7 +4,9 @@ module.exports = {
         const player2_hands = c2.split(',')
         if (c1 === c2)
             return 'Tie';
-        // if (player1_hands[4])
+        if(player1_hands[4] > player2_hands[4]){
+            return "Black wins"
+        }
         return "White wins"
     }
 }
