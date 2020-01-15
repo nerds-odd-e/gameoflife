51dab7db3a674f49b8cef37c97b7060b8bb724fa
Joey Chen <jay.hatelove@gmail.com>
2020-01-15 11:02:45 +1100

    working

--- src/poker.test.js
+++ src/poker.test.js
@@ -2,7 +2,7 @@ let poker =require('./poker.js')
 
 describe('Pocker hand', () => {
   it('both playes have four of a kind', () => {
-    // expect(poker.check('S2,H2,C2,D2,D3', 'S2,H2,C2,D2,D3')).toBe('Tie')
+    expect(poker.check('S2,H2,C2,D2,D3', 'S2,H2,C2,D2,D3')).toBe('Tie')
   })
 
 })
