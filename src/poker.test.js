5c600c0068c519bb0e815635f9d0e7d656a441d2
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2020-01-15 08:13:45 +0800

    empty message

--- src/poker.test.js
+++ src/poker.test.js
@@ -2,7 +2,7 @@ let poker =require('./poker.js')
 
 describe('Pocker hand', () => {
   it('both playes have four of a kind', () => {
-    expect(poker.check('S2,H2,C2,D2,D3', 'S2,H2,C2,D2,D3')).toBe('Tie')
+    expect(poker.check('2S,2H,2C,2D,3D', '2S,2H,2C,2D,3D')).toBe('Tie')
   })
 
   it('High card blank win', () => {
