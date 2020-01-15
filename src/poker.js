8ce0117b42a51658743c7e4c805842e29040ac9e
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2020-01-15 08:52:45 +0800

    empty message

--- src/poker.js
+++ src/poker.js
@@ -2,9 +2,10 @@ const card = (symbol) => {
     return {suit: symbol[1], value: symbol[0]}
 }
 
-let getPlayer1Hands = function (c1) {
+const getHands = function (c1) {
     return c1.split(',').sort();
 };
+
 module.exports = {
     singleCardWins: function (player1_hands, player2_hands, index = 3) {
         if (player1_hands[index] > player2_hands[index]) {
@@ -12,7 +13,7 @@ module.exports = {
         }
         return "White wins"
     }, check(c1, c2) {
-        const player1_hands = getPlayer1Hands(c1)
+        const player1_hands = getHands(c1)
         const player2_hands = c2.split(',').sort()
         if (c1 === c2)
             return 'Tie';
