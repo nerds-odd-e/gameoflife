81f0d9196596f397bd7fb73ec973b83649312eec
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2020-01-15 08:37:05 +0800

    empty message

--- src/poker.test.js
+++ src/poker.test.js
@@ -17,12 +17,16 @@ describe('Pocker hand', () => {
     it('High card black win by comparing number only', () => {
       expect(poker.check('3S,2S,4S,5S,7C', '3S,2S,4S,5S,6D')).toBe('Black wins')
     })
+
+    it('High card black win by sort', () => {
+      expect(poker.check('3S,2S,7C,4S,5S', '3S,2S,4S,5S,6D')).toBe('Black wins')
+    })
   })
 
   it('both players have four of a kind', () => {
     expect(poker.check('2S,2H,2C,2D,3D', '2S,2H,2C,2D,3D')).toBe('Tie')
   })
-    
+
   it('one player has a pair and the other does not', () => {
 //    expect(poker.check('2S,2H,3C,5D,7D', '3S,5H,7C,9D,QD')).toBe('Black wins')
   })
