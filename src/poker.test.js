2a7cae48e1ab1761b5c3524a0ee2631d848808b5
Joey Chen <jay.hatelove@gmail.com>
2020-01-15 11:00:50 +1100

    working

---
+++ src/poker.test.js
@@ -0,0 +1,8 @@
+let poker =require('./fizzbuzz.js')
+
+describe('Pocker hand', () => {
+  it('both playes have four of a kind', () => {
+    // expect(poker.check('S2,H2,C2,D2,D3', 'S2,H2,C2,D2,D3')).toBe('Tie')
+  })
+
+})
