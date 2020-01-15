990ffb11545a085bd4a70c0dc266f884c8d56306
wjmmx <philip.wj@gmail.com>
2020-01-15 11:30:55 +1100

    working

--- src/poker.test.js
+++ src/poker.test.js
@@ -15,8 +15,10 @@ describe('Pocker hand', () => {
     })
   })
 
-  it('both players have four of a kind', () => {
-    expect(poker.check('2S,2H,2C,2D,3D', '2S,2H,2C,2D,3D')).toBe('Tie')
+  describe('Tie', ()=>{
+    it('both players have four of a kind', () => {
+      expect(poker.check('2S,2H,2C,2D,3D', '2S,2H,2C,2D,3D')).toBe('Tie')
+    })
   })
     
   it('one player has a pair and the other does not', () => {
