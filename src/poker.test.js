fa0fcd8192b3c698dbfb8edf8b41b5069976a19b
Terry Yin <terry.yinzhe@gmail.com>
2020-01-15 11:26:42 +1100

    working

--- src/poker.test.js
+++ src/poker.test.js
@@ -1,16 +1,18 @@
 let poker =require('./poker.js')
 
 describe('Pocker hand', () => {
-  it('High card white win', () => {
-    expect(poker.check('3S,2S,4S,5S,6D', '3S,2S,4S,5S,7D')).toBe('White wins')
-  })
+  describe('High card', () => {
+    it('High card white win', () => {
+      expect(poker.check('3S,2S,4S,5S,6D', '3S,2S,4S,5S,7D')).toBe('White wins')
+    })
 
-  it('High card black win', () => {
-    expect(poker.check('3S,2S,4S,5S,7D', '3S,2S,4S,5S,6D')).toBe('Black wins')
-  })
+    it('High card black win', () => {
+      expect(poker.check('3S,2S,4S,5S,7D', '3S,2S,4S,5S,6D')).toBe('Black wins')
+    })
 
-  it('High card black win by comparing number only', () => {
-    expect(poker.check('3S,2S,4S,5S,7C', '3S,2S,4S,5S,6D')).toBe('Black wins')
+    it('High card black win by comparing number only', () => {
+      expect(poker.check('3S,2S,4S,5S,7C', '3S,2S,4S,5S,6D')).toBe('Black wins')
+    })
   })
 
   it('both players have four of a kind', () => {
