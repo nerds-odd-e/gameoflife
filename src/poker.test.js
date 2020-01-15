9fb66c39ea9b76e3f66c875845aaa30a2345333b
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2020-01-15 08:10:49 +0800

    empty message

--- src/poker.test.js
+++ src/poker.test.js
@@ -5,4 +5,8 @@ describe('Pocker hand', () => {
     expect(poker.check('S2,H2,C2,D2,D3', 'S2,H2,C2,D2,D3')).toBe('Tie')
   })
 
+  it('High card blank win', () => {
+    // expect(poker.check('3S,2S,4S,5S,6D', '3S,2S,4S,5S,7D')).toBe('Tie')
+  })
+
 })
