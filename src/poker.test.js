deb896de68441672f57a676966551f9657df5de7
Terry Yin <terry.yinzhe@gmail.com>
2020-01-15 11:30:58 +1100

    working

--- src/poker.test.js
+++ src/poker.test.js
@@ -18,4 +18,7 @@ describe('Pocker hand', () => {
   it('both players have four of a kind', () => {
     expect(poker.check('2S,2H,2C,2D,3D', '2S,2H,2C,2D,3D')).toBe('Tie')
   })
+    
+  it('one player has a pair and the other does not', () => {
+  })
 })
