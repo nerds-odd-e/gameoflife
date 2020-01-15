a10ca1706e18d22f3cc30065d19b7bce9e3f5b0c
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2020-01-15 08:53:54 +0800

    empty message

--- src/poker.js
+++ src/poker.js
@@ -14,16 +14,16 @@ module.exports = {
         return "White wins"
     }, check(black, white) {
         const black_hands = getHands(black)
-        const player2_hands = getHands(white)
+        const white_hands = getHands(white)
         if (black === white)
             return 'Tie';
-        if (black_hands[4] === player2_hands[4]) {
-            return this.singleCardWins(black_hands, player2_hands, 3);
+        if (black_hands[4] === white_hands[4]) {
+            return this.singleCardWins(black_hands, white_hands, 3);
         }
         if(black_hands[0][0] === black_hands[1][0]){
             return "Black wins"
         }
-        return this.singleCardWins(black_hands, player2_hands, 4)
+        return this.singleCardWins(black_hands, white_hands, 4)
     },
     card(symbol){
         return {suit: symbol[1], value: symbol[0]}
