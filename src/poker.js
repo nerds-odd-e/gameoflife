f65b4ab24f73f644d3f2e5482ab2b4b67dcb8b10
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2020-01-15 08:32:50 +0800

    empty message

--- src/poker.js
+++ src/poker.js
@@ -1,7 +1,7 @@
 module.exports = {
     check(c1, c2) {
-        const player1_hands = c1.split(',')
-        const player2_hands = c2.split(',')
+        const player1_hands = c1.split(',').sort()
+        const player2_hands = c2.split(',').sort()
         if (c1 === c2)
             return 'Tie';
         if(player1_hands[4] > player2_hands[4]){
--- src/poker.test.js
+++ src/poker.test.js
@@ -13,6 +13,10 @@ describe('Pocker hand', () => {
     it('High card black win by comparing number only', () => {
       expect(poker.check('3S,2S,4S,5S,7C', '3S,2S,4S,5S,6D')).toBe('Black wins')
     })
+
+    it('High card black win by sorting', () => {
+      expect(poker.check('3S,2S,7C,4S,5S', '3S,2S,4S,5S,6D')).toBe('Black wins')
+    })
   })
 
   describe('Tie', ()=>{
@@ -20,7 +24,7 @@ describe('Pocker hand', () => {
       expect(poker.check('2S,2H,2C,2D,3D', '2S,2H,2C,2D,3D')).toBe('Tie')
     })
   })
-    
+
   it('one player has a pair and the other does not', () => {
   })
 })
